5cd7feecaeaf638c0a3f2fb2e2f6e430014b7aed
TrinhDuc01 <ductrinh.0301@gmail.com>
2024-11-14 16:36:37 +0700

    create foreground

---
+++ src/components/background/Foreground.js
@@ -0,0 +1,11 @@
+const Foreground = () => {
+    const style = {
+        height: '100vh',
+        backgroundImage: "linear-gradient(#ec7686 5%, #fab500 40%)" //se nhan tu redux de thay doi nen
+    }
+    return (<div style={style}>
+
+    </div>)
+}
+
+export default Foreground
