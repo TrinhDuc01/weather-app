54cb232befe32b7819285eecc2b904fdf9dff127
TrinhDuc01 <ductrinh.0301@gmail.com>
2024-11-15 15:53:21 +0700

    create ground

---
+++ src/components/background/Ground.js
@@ -0,0 +1,17 @@
+const Ground=()=>{
+    const style ={
+        width:"200vw",
+        height:"100vw",
+        borderRadius:"50%",
+        backgroundColor:"#fff",
+        position:"absolute",
+        right:"50%",
+        transform: "translateX(50%)",
+        top:"55%"
+    }
+    return <div style={style}>
+
+    </div>
+}
+
+export default Ground
