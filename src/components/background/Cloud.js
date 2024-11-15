536264d692134876d4ee51a188fe3d3eabfc7fc7
TrinhDuc01 <ductrinh.0301@gmail.com>
2024-11-15 15:52:42 +0700

    create cloud

---
+++ src/components/background/Cloud.js
@@ -0,0 +1,17 @@
+const Cloud = ({top,left}) =>{
+    const style = {
+        width:"6.5%",
+        height:"5vw",
+        position:"absolute",
+        top:top,
+        left:left,
+        background:'#f7c098',
+        borderRadius: '50%',
+        boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
+    }
+    return(<div style={style}>
+
+    </div>)
+}
+
+export default Cloud
